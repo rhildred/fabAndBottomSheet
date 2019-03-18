4151ad0de9058f0596be8c7c890c0ed3418eb5a4
Rich Hildred <rhildred>
2019-03-18 14:19:11 +0000

    initial commit

---
+++ public/main.js
@@ -0,0 +1,6 @@
+import "materialize-css"
+
+ document.addEventListener('DOMContentLoaded', () => {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
